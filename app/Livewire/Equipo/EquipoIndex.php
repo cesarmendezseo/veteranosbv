540eb6ba567c8bca0661c0bbb53c42b7e6048f0e
Cesar Mendez <cesarmendez.seo@gmail.com>
2025-07-27 18:24:54 -0300

    instalacion de SweetAlert + livewire alert

--- app/Livewire/Equipo/EquipoIndex.php
+++ app/Livewire/Equipo/EquipoIndex.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire\Equipo;
 
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Component;
 
 class EquipoIndex extends Component
 {
+
+
     public function render()
     {
         return view('livewire.equipo.equipo-index');
--- resources/views/livewire/equipo/equipo-index.blade.php
+++ resources/views/livewire/equipo/equipo-index.blade.php
@@ -1,6 +1,4 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    <h1 class="dark:text-white">Ver</h1>
 
 
 </div>
