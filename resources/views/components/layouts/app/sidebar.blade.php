a7a1c9ccc9c1f8ce06853cc33525a212c35b0adf
Cesar Mendez <cesarmendez.seo@gmail.com>
2025-08-17 23:43:53 -0300

    roles y permisos

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -36,8 +36,9 @@
                     <flux:navlist.item icon="shield-check" :href="route('categoria.index')" :current="request()->routeIs('categoria.index')" wire:navigate>{{ __('Categoria') }}</flux:navlist.item>
                     <flux:navlist.item icon="device-phone-mobile" :href="route('canchas.index')" :current="request()->routeIs('Canchas.index')" wire:navigate>{{ __('Canchas') }}</flux:navlist.item>
                 </flux:navlist.group>
+                @role('role:admin')
                 <flux:navlist.item icon="shield-check" :href="route('rol.panel')" :current="request()->routeIs('rol.panel')" wire:navigate>{{ __('Roles y Permisos') }}</flux:navlist.item>
-
+                @endrole
 
             </flux:navlist.group>
         </flux:navlist>
--- routes/web.php
+++ routes/web.php
@@ -59,64 +59,64 @@ Route::middleware(['auth'])->group(function () {
     //=========================ROLES Y PERMISOS================================
 
     Route::get('/equipo', EquipoIndex::class)->name('equipo.index');
-    Route::get('/equipo/crear', EquipoCrear::class)->name('equipo.crear');
-    Route::get('/equipo/{equipoId}/editar', EquipoEditar::class)->name('equipo.editar');
-    Route::get('/equipo/{equipoId}/logo', [LogoEquipoController::class, 'upload'])->name('equipo.logo.upload');
-    Route::post('/equipo/{equipoId}/logo', [LogoEquipoController::class, 'guardarLogo'])->name('equipo.logo.guardar');
+    Route::get('/equipo/crear', EquipoCrear::class)->name('equipo.crear')->middleware('role:admin');
+    Route::get('/equipo/{equipoId}/editar', EquipoEditar::class)->name('equipo.editar')->middleware('role:admin');
+    Route::get('/equipo/{equipoId}/logo', [LogoEquipoController::class, 'upload'])->name('equipo.logo.upload')->middleware('role:admin');
+    Route::post('/equipo/{equipoId}/logo', [LogoEquipoController::class, 'guardarLogo'])->name('equipo.logo.guardar')->middleware('role:admin');
     Route::get('/equipo/listado-buena-fe', ListadoBuenaFe::class)->name('listado-buena-fe');
 
     //==============JUGADORES========================
     Route::get('/jugadores', JugadoresIndex::class)->name('jugadores.index');
-    Route::get('/jugadores/{jugadorId}/editar', JugadoresEditar::class)->name('jugadores.editar');
-    Route::get('/jugadores/crear', JugadoresCrear::class)->name('jugadores.crear');
-    Route::get('/jugadores/{jugadorId}/foto', [FotoJugadorController::class, 'upload'])->name('jugadores.foto.upload');
-    Route::post('/jugadores/{jugadorId}/foto', [FotoJugadorController::class, 'guardarFoto'])->name('jugadores.foto.guardar');
+    Route::get('/jugadores/{jugadorId}/editar', JugadoresEditar::class)->name('jugadores.editar')->middleware('role:admin|comision');
+    Route::get('/jugadores/crear', JugadoresCrear::class)->name('jugadores.crear')->middleware('role:admin|comision');
+    Route::get('/jugadores/{jugadorId}/foto', [FotoJugadorController::class, 'upload'])->name('jugadores.foto.upload')->middleware('role:admin|comision');
+    Route::post('/jugadores/{jugadorId}/foto', [FotoJugadorController::class, 'guardarFoto'])->name('jugadores.foto.guardar')->middleware('role:admin|comision');
     //==============FIN JUGADORES========================
     //==============CAMPEONATO========================
     Route::get('/campeonato', CampeonatoIndex::class)->name('campeonato.index');
-    Route::get('/campeonato/crear', CampeonatoCrear::class)->name('campeonato.crear');
-    Route::get('/campeonato/{campeonatoId}/editar', CampeonatoEditar::class)->name('campeonato.editar');
+    Route::get('/campeonato/crear', CampeonatoCrear::class)->name('campeonato.crear')->middleware('role:admin|comision');
+    Route::get('/campeonato/{campeonatoId}/editar', CampeonatoEditar::class)->name('campeonato.editar')->middleware('role:admin|comision');
     // Route::get('/campeonato/{campeonatoId}/logo', [LogoCampeonatoController::class, 'upload'])->name('campeonato.logo.upload');
     // Route::post('/campeonato/{campeonatoId}/logo', [LogoCampeonatoController::class, 'guardarLogo'])->name('campeonato.logo.guardar');
     //==============FIN CAMPEONATO========================
     //==============CATEGORIA========================
     Route::get('/categoria', \App\Livewire\Categoria\CategoriaIndex::class)->name('categoria.index');
-    Route::get('/categoria/crear', CategoriaCrear::class)->name('categoria.crear');
-    Route::get('/categoria/{categoriaId}/editar', CategoriaEdit::class)->name('categoria.editar');
+    Route::get('/categoria/crear', CategoriaCrear::class)->name('categoria.crear')->middleware('role:admin|comision');
+    Route::get('/categoria/{categoriaId}/editar', CategoriaEdit::class)->name('categoria.editar')->middleware('role:admin|comision');
     //==============FIN CATEGORIA========================  
     //==============FIN ESTADIOS======================== 
     Route::get('/estadios', CanchasIndex::class)->name('canchas.index');
-    Route::get('/estadios/crear', CanchasCrear::class)->name('canchas.crear');
-    Route::get('/estadios/{estadioId}/editar', CanchasEditar::class)->name('canchas.editar');
+    Route::get('/estadios/crear', CanchasCrear::class)->name('canchas.crear')->middleware('role:admin|comision');
+    Route::get('/estadios/{estadioId}/editar', CanchasEditar::class)->name('canchas.editar')->middleware('role:admin|comision');
     //==============FIN ESTADIOS========================
     //==============FIXTURE======================== 
     Route::get('/fixture', FixtureIndex::class)->name('fixture.index');
-    Route::get('/fixture/{campeonatoId}/crear', FixtureCrear::class)->name('fixture.crear');
+    Route::get('/fixture/{campeonatoId}/crear', FixtureCrear::class)->name('fixture.crear')->middleware('role:admin|comision');
     Route::get('/fixture/{campeonatoId}/ver', FixtureVer::class)->name('fixture.ver');
-    Route::get('/fixture/{estadioId}/editar', FixtureEditar::class)->name('fixture.editar');
-    Route::get('/fixture/automatico', \App\Livewire\Fixture\FixtureAutomatico::class)->name('fixture.automatico');
+    Route::get('/fixture/{estadioId}/editar', FixtureEditar::class)->name('fixture.editar')->middleware('role:admin|comision');
+    Route::get('/fixture/automatico', \App\Livewire\Fixture\FixtureAutomatico::class)->name('fixture.automatico')->middleware('role:admin');
     //==============FIN FIXTURE========================
     //==============ASIGNAR EQUIPOS========================
-    Route::get('/campeonato/{campeonatoId}/asignar-equipos', AsignarEquipos::class)->name('asignar-equipos');
+    Route::get('/campeonato/{campeonatoId}/asignar-equipos', AsignarEquipos::class)->name('asignar-equipos')->middleware('role:admin');
     //==============FIN ASIGNAR EQUIPOS========================
     //==============TABLA DE POSICIONES========================
     Route::get('/tabla-posiciones', TablaPosicionIndex::class)->name('tabla-posiciones');
     Route::get('/tabla-posiciones/{campeonatoId}/ver', \App\Livewire\TablaPosicion\TablaPosiciones::class)->name('tabla-posiciones.ver');
-    Route::get('/tabla-posiciones/{campeonato}/tabla-pdf', [TablaPosiciones::class, 'generarTablaPosicionesPDF'])->name('tabla.pdf');
-    Route::get('/exportar-encuentros', [FixtureCrear::class, 'exportar'])->name('encuentros.exportar');
+    Route::get('/tabla-posiciones/{campeonato}/tabla-pdf', [TablaPosiciones::class, 'generarTablaPosicionesPDF'])->name('tabla.pdf')->middleware('role:admin|comision');
+    Route::get('/exportar-encuentros', [FixtureCrear::class, 'exportar'])->name('encuentros.exportar')->middleware('role:admin|comision');
     //==============FIN TABLA DE POSICIONES========================
 
     //==============SANCIONES========================
     Route::get('/sanciones', SancionesIndex::class)->name('sanciones.index');
-    Route::get('/sanciones/{campeonatoId}/crear', \App\Livewire\Sanciones\SancionesCrear::class)->name('sanciones.crear');
+    Route::get('/sanciones/{campeonatoId}/crear', \App\Livewire\Sanciones\SancionesCrear::class)->name('sanciones.crear')->middleware('role:admin|comision');
     Route::get('/sanciones/actualizar-cumplimientos', [\App\Livewire\Sanciones\SancionesCrear::class, 'actualizarCumplimientosSanciones'])->name('sanciones.actualizar-cumplimientos');
     //==============FIN SANCIONES========================
     //==============ESTADISTICA========================
     Route::get('/estadistica', \App\Livewire\Estadistica\EstadisticaIndex::class)->name('estadistica.index');
-    Route::get('/estadistica/{campeonatoId}/ver', \App\Livewire\Estadistica\EstadisticaVer::class)->name('estadistica.ver');
+    Route::get('/estadistica/{campeonatoId}/ver', \App\Livewire\Estadistica\EstadisticaVer::class)->name('estadistica.ver')->middleware('role:admin|comision');
     //==============FIN ESTADISTICA========================
     //==============ALTAS Y BAJAS========================
-    Route::get('/altas-bajas', \App\Livewire\AltasBajas\AltasBajasIndex::class)->name('altas-bajas.index');
+    Route::get('/altas-bajas', \App\Livewire\AltasBajas\AltasBajasIndex::class)->name('altas-bajas.index')->middleware('role:admin');;
 });
 
 require __DIR__ . '/auth.php';
