bc363e9b10153e9e53aa70c6397dfdc4a2897d9f
Cesar Mendez <cesarmendez.seo@gmail.com>
2025-09-11 10:53:18 -0300

    Reemplazando el historial remoto

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     @include('partials.head')
+     
 </head>
 
 <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
--- resources/views/livewire/jugadore/jugadores-index.blade.php
+++ resources/views/livewire/jugadore/jugadores-index.blade.php
@@ -51,27 +51,7 @@
                              <th scope="row"
                                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                  @if ($jugador->foto)
-                                     <div class="mb-4 flex justify-center">
-                                         <div
-                                             class="w-18 h-18 rounded-full border-4 border-gray-300 shadow-lg overflow-hidden">
-                                             <img src="{{ asset('storage/' . $jugador->foto) }}"
-                                                 alt="Logo {{ $jugador->nombre }}" class="w-full h-full object-cover" />
-                                         </div>
-                                     </div>
-                                 @else
-                                     <div
-                                         class="w-18 h-18 rounded-full border-4 border-gray-300 shadow-lg overflow-hidden flex items-center justify-center">
-                                         <div
-                                             class="flex animate-pulse  items-center justify-center w-full h-48 bg-gray-300 rounded-sm sm:w-96 dark:bg-gray-700">
-                                             <svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                 viewBox="0 0 20 18">
-                                                 <path
-                                                     d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                                             </svg>
-                                         </div>
-                                     </div>
-                                 @endif
+                         
                              </th>
                              <th scope=" row"
                                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
