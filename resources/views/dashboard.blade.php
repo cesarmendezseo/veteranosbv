39d9a7cedf626e35627e7ec5b885afcb37787f8f
Cesar Mendez <cesarmendez.seo@gmail.com>
2025-09-13 19:52:18 -0300

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,79 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-        </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 bg-slate-200 p-4 rounded-2xl shadow-2xl">
+            <!-- Tarjeta Jugadores -->
+            <a href="{{ route('jugadores.index') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Jugadores</h2>
+                </div>
+                <div>
+                    <img src="{{ asset('images/jugador.jpg') }}" alt="Logo" class="w-20 h-20 rounded-xl">
+                </div>
+            </a>
+
+
+            <!-- Tarjeta Fixture -->
+            <a href="{{ route('fixture.index') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Fixture</h2>
+
+                </div>
+                <div ">
+     <img src=" {{ asset('images/fixture.png') }}" alt="Logo" class="w-20 h-20 ">
+                </div>
+            </a>
+
+            <!-- Tarjeta Estadísticas -->
+            <a href="{{ route('sanciones.index') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Estadísticas</h2>
+
+                </div>
+                <div ">
+     <img src=" {{ asset('images/estadistica.png') }}" alt="Logo" class="w-20 h-20 rounded-2xl">
+                </div>
+            </a>
+            <!-- Listado Buena Fe -->
+            <a href="{{ route('listado-buena-fe') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Listado Buena Fe</h2>
+
+                </div>
+                <div ">
+     <img src=" {{ asset('images/listado.png') }}" alt="Logo" class="w-20 h-20 rounded-2xl">
+                </div>
+            </a>
+            <!-- Tabla posicion -->
+            <a href="{{ route('tabla-posiciones') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Tabla de Posición</h2>
+
+                </div>
+                <div ">
+     <img src=" {{ asset('images/tabla-posicion.png') }}" alt="Logo" class="w-20 h-20 rounded-2xl">
+                </div>
+            </a>
+            <!-- Altas y Bajas -->
+            @adminOrCan('comision')
+            <a href="{{ route('altas-bajas.index') }}" class="bg-white rounded-2xl shadow-md p-6 flex items-center justify-between 
+                    hover:shadow-xl hover:scale-105 transition transform duration-300 ease-in-out">
+                <div>
+                    <h2 class="text-lg font-semibold text-gray-700">Altas y Bajas</h2>
+
+                </div>
+                <div ">
+     <img src=" {{ asset('images/altas-bajas.png') }}" alt="Logo" class="w-20 h-20 rounded-2xl">
+                </div>
+            </a>
+            @endadminOrCan
+
+            <!-- Puedes seguir agregando más tarjetas según las opciones del menú -->
         </div>
     </div>
 </x-layouts.app>
