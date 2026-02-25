65396db8267995ec897655be4fa45c34dfa786c1
Cesar Mendez <cesarmendez.seo@gmail.com>
2026-02-25 21:16:27 +0000

    arreglado actualizaciones sancion automatico

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sanciones:actualizar')->weeklyOn(1, '00:00'); // lunes 00:00
+Schedule::command('sanciones:actualizar')->weeklyOn(0, '23:59'); // lunes 00:00
 
 //Schedule::command('sanciones:actualizar')->everyMinute(); // PARA PRUEBAS SOLO, LUEGO COMENTAR O ELIMINAR
