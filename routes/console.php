a7a9d7e2ce28b913ecb2348833e2f36ddd6f2b8c
Cesar Mendez <cesarmendez.seo@gmail.com>
2026-03-02 14:22:50 +0000

    arreglando cron

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('sanciones:actualizar')->weeklyOn(0, '23:59'); // lunes 00:00
+//Schedule::command('sanciones:actualizar')->weeklyOn(0, '23:59'); // lunes 00:00
+Schedule::command('sanciones:actualizar')->everyMinute();
 
 //Schedule::command('sanciones:actualizar')->everyMinute(); // PARA PRUEBAS SOLO, LUEGO COMENTAR O ELIMINAR
